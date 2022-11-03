737b5f94234dfc3dc85c90a84c4f1d0f17748d1d
Jandiee <jandek.martin@gmail.com>
2022-11-03 15:06:01 +0100

    install axios and react query

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,16 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { StyledEngineProvider } from "@mui/material";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <StyledEngineProvider injectFirst>
-      <Component {...pageProps} />
+      <QueryClientProvider client={queryClient}>
+        <Component {...pageProps} />
+      </QueryClientProvider>
     </StyledEngineProvider>
   );
 }
