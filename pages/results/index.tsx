9eabcf1d18f4004695697381ffd088b4dcfc5477
Jandiee <jandek.martin@gmail.com>
2023-01-18 10:36:03 +0100

    minor changes on results page

--- pages/results/index.tsx
+++ pages/results/index.tsx
@@ -1,22 +1,12 @@
-import {
-  List,
-  ListItem,
-  ListItemButton,
-  ListItemText,
-  Typography,
-} from "@mui/material";
+import { List, ListItem, ListItemButton, ListItemText } from "@mui/material";
 import Link from "next/link";
-import React, { useEffect } from "react";
+import React from "react";
 import AppBar from "../../components/common/AppBar";
 import ContentWrapper from "../../components/common/layout/ContentWrapper";
 import { useParticipants } from "../../hooks/participants/useParticipants";
 
 const ResultsPage = () => {
   const { data, isLoading, isError } = useParticipants();
-
-  useEffect(() => {
-    console.log(data);
-  }, [data]);
 
   if (isLoading || isError) {
     return null;
@@ -31,7 +21,7 @@ const ResultsPage = () => {
             <React.Fragment key={item.participant}>
               {item.links.map((link, idx) => {
                 return (
-                  <ListItem key={link[idx]} component={Link} href={link[idx]}>
+                  <ListItem key={link[idx]}>
                     <ListItemButton component={Link} href={link[idx]}>
                       <ListItemText>
                         Experiment: {item.participantExperiments[idx].title},
