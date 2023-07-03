311dd7821a18fc31b5557067153e7bdef7a9256c
Jandiee <jandek.martin@gmail.com>
2023-07-03 10:48:13 +0200

    fix next version

--- middleware.ts
+++ middleware.ts
@@ -4,7 +4,7 @@ import type { NextRequest } from "next/server";
 export function middleware(request: NextRequest) {
   request.headers.set(
     "Authorization",
-    `Bearer ${request.cookies.get("accessToken")?.["value"]}` || ""
+    `Bearer ${request.cookies.get("accessToken")?.["value"]}`
   );
 
   return NextResponse.next({ request: { headers: request.headers } });
